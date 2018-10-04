1cbf176e07d3ef25696df9cced009c893559d8c5
Gabriel Abarca Aguilar <gababarca@hotmail.com>
2018-10-03 23:42:32 -0600

    Spawns Minions

---
+++ League of Gems (Unity)/Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	public Color color;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.color = color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move(new Vector3(4f, 0f, 0f) );
+	}
+    
+	public void Move(Vector3 new_position){
+		transform.Translate(new_position* Time.deltaTime);	
+	}
+}
---
+++ League of Gems (Unity)/Assets/Scripts/Spawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+	public GameObject minions;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 15; i++){
+			GameObject minion = Instantiate(minions, transform.position, transform.rotation);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
