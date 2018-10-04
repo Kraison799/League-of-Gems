79602e93fc8cf6f27483648b59d2fce3d7f6a8e0
Gabriel Abarca Aguilar <gababarca@hotmail.com>
2018-10-03 22:22:31 -0600

    Packaged on Unity
    Unity project now has everything separated

---
+++ League of Gems (Unity)/Assets/Scripts/TextEdit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+
+public class TextEdit : MonoBehaviour {
+	[DllImport("liblib.so")]
+	static extern int Add(int a, int b);
+	Text text;
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = Add(4, 2).ToString();
+	}
+}
