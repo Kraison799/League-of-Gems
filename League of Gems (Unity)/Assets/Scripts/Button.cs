a34185c150402b5bacee8ec65531249ea05d65ca
Gabriel Abarca Aguilar <gababarca@hotmail.com>
2018-11-12 21:12:50 -0600

    UI

---
+++ League of Gems (Unity)/Assets/Scripts/Button.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public void Event (int btn){
+        if (btn==1)
+        {
+            Debug.Log("boton 1");
+        }
+        if (btn == 2)
+        {
+            Debug.Log("boton 2");
+        }
+        if (btn == 3)
+        {
+            Debug.Log("boton 3");
+        }
+        if (btn == 4)
+        {
+            Debug.Log("boton 4");
+        }
+        if (btn == 5)
+        {
+            Debug.Log("boton 5");
+        }
+        if (btn == 6)
+        {
+            Debug.Log("boton 6");
+        }
+        if (btn == 7)
+        {
+            Debug.Log("boton 7");
+        }
+        if (btn == 8)
+        {
+            Debug.Log("boton 8");
+        }
+    }
+    
+}
