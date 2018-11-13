92e126c38a404948e670d66eb1841b287f4e0a19
Maria Avila <mariafer50121@gmail.com>
2018-11-12 23:37:25 -0600

    Added life bar to gems

--- League of Gems (Unity)/Assets/Scripts/EnemyGem.cs
+++ League of Gems (Unity)/Assets/Scripts/EnemyGem.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyGem : Gem {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    int MaxHp = 500;
+    int Hp = 500;
+    public SimpleHealthBar healthBar;
+    public GameObject EnemyFire;
+    public GameObject fire;
+
+    // Use this for initialization
+    void Start () {
+        healthBar.UpdateBar(Hp, MaxHp);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Hp <= 0)
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);//reinicio lvl
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals(fire.tag))
+        {
+            healthBar.UpdateBar(Hp -= 10, MaxHp);
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag.Equals(EnemyFire.tag))
+        {
+            Physics.IgnoreCollision(GetComponent<Collider>(), collision.gameObject.GetComponent<Collider>());
+        }
+    }
 }
